60e919578781f778eb9ece890bb78affee1d9cc6
matgd <matgd@users.noreply.github.com>
2022-12-04 13:18:25 +0100

    Day3 in progress

---
+++ day03/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/matgd/advent2022/utils"
+)
+
+func getCharPriorities() map[string]int {
+	priorities := map[string]int{}
+
+	currentPriority := 0
+	for ch := 'a'; ch <= 'z'; ch++ {
+		priorities[string(ch)] = currentPriority
+		currentPriority++
+	}
+
+	for ch := 'A'; ch <= 'Z'; ch++ {
+		priorities[string(ch)] = currentPriority
+		currentPriority++
+	}
+
+	return priorities
+}
+
+func splitRacksack(input string) (string, string) {
+	first := input[:len(input)/2]
+	second := input[len(input)/2:]
+
+	return first, second
+}
+
+func getDuplicatePriority(firstRacksack, secondRacksack string) int {
+	occurences := map[string]int{}
+
+}
+
+func Task1(inputFile string) int {
+	lines := utils.ReadFileLines(inputFile)
+
+	for _, line := range lines {
+		first, second := splitRacksack(line)
+	}
+
+	return 0
+}
+
+func main() {
+	fmt.Println("[Task 1]", Task1("input.txt"))
+}
