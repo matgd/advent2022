208ca2833d39739736dd822026c09f967ad80417
matgd <28411337+matgd@users.noreply.github.com>
2022-12-09 20:17:08 +0100

    Day 09

--- day09/main_test.go
+++ day09/main_test.go
@@ -14,10 +14,10 @@ func TestPart1(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
-	// got := Day9_2("input_example.txt")
-	// var expected uint64 = 8
-	// if got != expected {
-	// t.Log("Got", got, " expected", expected)
-	// t.Fail()
-	// }
+	got := Day9p2("input_example_bigger.txt")
+	var expected uint64 = 36
+	if got != expected {
+		t.Log("Got", got, " expected", expected)
+		t.Fail()
+	}
 }
