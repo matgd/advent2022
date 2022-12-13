c1406ebae65a0c3f96894b44c22fefd4962897f2
matgd <matgd@users.noreply.github.com>
2022-12-13 19:06:06 +0100

    Day 13, pt. 2

--- day13/main.py
+++ day13/main.py
@@ -56,9 +56,27 @@ def day13(input_data: list[list[str]]):
         if in_order(packet_A, packet_B):
             in_right_order.append(index)
 
-    print(sum(in_right_order))
+    print("Day 13, pt. 1:", sum(in_right_order))
+
+def day13p2(input_data: list[list[str]]):
+    packets = []
+    for line in input_data:
+        if line:
+            packets.append(json.loads(line))
+    packets.append([[2]])
+    packets.append([[6]])
+    
+    # Bubble sort
+    for i in range(len(packets)):
+        for j in range(len(packets) - i - 1):
+            if not in_order(packets[j], packets[j+1]):
+                packets[j], packets[j+1] = packets[j+1], packets[j]
+
+    print("Day 13, pt. 2:", (packets.index([[2]])+1) * (packets.index([[6]])+1))
+
+
 
 if __name__ == "__main__":
-    day13(read_lines(SCRIPT_DIR / "input_example.txt"))
     day13(read_lines(SCRIPT_DIR / "input.txt"))
+    day13p2(read_lines(SCRIPT_DIR / "input.txt"))
     
